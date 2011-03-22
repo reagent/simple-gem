34f52a86a3558005589398885982f2021c7d4ab2
Patrick Reagan <reaganpr@gmail.com>
2011-03-21 22:09:35 -0600

    Bump to 0.6.0

--- lib/simple_gem/version.rb
+++ lib/simple_gem/version.rb
@@ -1,13 +1,13 @@
 module SimpleGem
   module Version
-    
+
     MAJOR = 0
-    MINOR = 5
-    TINY  = 1
-    
+    MINOR = 6
+    TINY  = 0
+
     def self.to_s
       [MAJOR, MINOR, TINY].join('.')
     end
-    
+
   end
 end
